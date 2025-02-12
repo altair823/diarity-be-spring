1b7ed99569696dfce3b4eee975e5d8d99a78cf2b
altair823 <dlsrks0734@gmail.com>
2025-02-12 18:16:29 +0900

    initial commit

--- src/main/java/me/diarity/diaritybespring/users/UsersController.java
+++ src/main/java/me/diarity/diaritybespring/users/UsersController.java
@@ -1,4 +1,13 @@
 package me.diarity.diaritybespring.users;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/users")
 public class UsersController {
+    @GetMapping()
+    public String index() {
+        return "Hello, World!";
+    }
 }
