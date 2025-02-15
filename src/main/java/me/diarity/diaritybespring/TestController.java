57449ca41031295c43a343ae3a72293670840bdf
altair823 <dlsrks0734@gmail.com>
2025-02-15 19:34:01 +0900

    cicd test

--- src/main/java/me/diarity/diaritybespring/TestController.java
+++ src/main/java/me/diarity/diaritybespring/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String index() {
-        return "Hello, World!";
+        return "Hello, World! version0.1 by Diarity";
     }
 }
